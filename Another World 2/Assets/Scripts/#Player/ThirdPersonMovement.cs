65d6a2804e7d48f8d7430ed4e179678600027a3b
Ramiro Gonzalez Castillo <ramirogonzalezcastillo@gmail.com>
2022-08-29 02:11:33 -0300

    player jump update

--- Another World 2/Assets/Scripts/#Player/ThirdPersonMovement.cs
+++ Another World 2/Assets/Scripts/#Player/ThirdPersonMovement.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
+    public float jumpSpeed = 15f;
+    Vector3 velocity;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
     public Transform cam;
     [SerializeField]
     float speed;
@@ -17,6 +25,12 @@ public class ThirdPersonMovement : MonoBehaviour
 
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2;
+            playerAnimator.SetBool("jumping", false);
+        }
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -43,6 +57,18 @@ public class ThirdPersonMovement : MonoBehaviour
         else {
             playerAnimator.SetBool("walking", false);
             playerAnimator.SetBool("running", false);
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        if(!isGrounded)
+        {
+            playerAnimator.SetBool("walking", false);
+            playerAnimator.SetBool("running", false);
+            playerAnimator.SetBool("jumping", true);
         }
     }
 }
